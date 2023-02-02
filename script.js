94538640044db03dda58aca9d59eb2bf8d6bfd28
Kasia <kasia.bondarava@gmail.com>
2023-02-02 17:02:03 -0500

    Search giving back data

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${secret.key}&page=1`;
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${secret.key}&query="`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${secret.key}&query="`;
+
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 // Get initial movies
 getMovies(API_URL);
@@ -11,3 +15,17 @@ async function getMovies(url) {
 
     console.log(data.results);
 }
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm && searchTerm !== "") {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = "";
+    } else {
+        window.location.reload();
+    }
+})
